c27500ee7e2461868c3734e5b6462fb744294ff0
Viraj Sonawane <88841137+Viraj324@users.noreply.github.com>
2023-06-04 18:03:25 +0530

    Update assign2.c

--- assign2.c
+++ assign2.c
@@ -1,1 +1,21 @@
+/*
+ * File:   assign2.c
+ * Author: ADMIN
+ *
+ * Created on 4 June, 2023, 5:57 PM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    int arr[10];
+    int sum = 0;
+    for(int i = 0;i<10;i++){
+        sum = sum + i;
+    }
+    TRISB = 0;
+    PORTB = sum;
+    return;
+}
 
